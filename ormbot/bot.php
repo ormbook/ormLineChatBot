069f23b24c5a9ca1de54ea92dcc044d2df74e584
ormbook <ormbook@gmail.com>
2017-01-11 00:07:30 +0700

    Update bot.php

--- ormbot/bot.php
+++ ormbot/bot.php
@@ -55,8 +55,8 @@ if (!is_null($events['events'])) {
 
 
 			// Build message to reply back
-			//$messages = ['type' => 'text','text' => $text];
-			$messages = ['type' => 'template','altText' => $text,'template'=> ['type'=>'confirm','text'=>'Are you sure?','actions'=>['type'=>'message','label'=>'yes','text'=>'yes'],[['type'=>'message','label'=>'no','text'=>'no']]]];
+			$messages = ['type' => 'text','text' => $text];
+			//$messages = ['type' => 'template','altText' => $text,'template'=> ['type'=>'confirm','text'=>'Are you sure?','actions'=>['type'=>'message','label'=>'yes','text'=>'yes'],[['type'=>'message','label'=>'no','text'=>'no']]]];
 
 
 			// Make a POST Request to Messaging API to reply to sender
