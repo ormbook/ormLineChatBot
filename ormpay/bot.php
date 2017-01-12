f58b5b109b24137b8f1120233db5d9bb1afc925a
ormbook <ormbook@gmail.com>
2017-01-12 22:19:16 +0700

    Update bot.php

--- ormpay/bot.php
+++ ormpay/bot.php
@@ -1,1 +1,104 @@
-test
+<?php
+$access_token = 'FB/on3LbJAJR/y4lLP2Xr2Hb7tFtdIAJhlg/+dD6dZWmSbkG8gWGAeGAlITTUs2PPquo70uO27P29wFKRGsTaTDQ+ZoguddbV+KaqWirgRGwBNjQkVXmztECOGgjkU61kTkVw7TI6U9+SQvL43P0BQdB04t89/1O/w1cDnyilFU=';
+
+// Get POST body content
+$content = file_get_contents('php://input');
+//$content='{  "events": [    {      "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",      "type": "message",      "timestamp": 1462629479859,      "source": {        "type": "user",        "userId": "U206d25c2ea6bd87c17655609a1c37cb8"      },      "message": {        "id": "325708",        "type": "text",        "text": "Hello, world"      }    },    {      "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",      "type": "follow",      "timestamp": 1462629479859,      "source": {        "type": "user",        "userId": "U206d25c2ea6bd87c17655609a1c37cb8"      }    }  ]}';
+// Parse JSON
+$events = json_decode($content, true);
+//print_r($events);
+/*
+{
+  "events": [
+    {
+      "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+      "type": "message",
+      "timestamp": 1462629479859,
+      "source": {
+        "type": "user",
+        "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+      },
+      "message": {
+        "id": "325708",
+        "type": "text",
+        "text": "Hello, world"
+      }
+    },
+    {
+      "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+      "type": "follow",
+      "timestamp": 1462629479859,
+      "source": {
+        "type": "user",
+        "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+      }
+    }
+  ]
+}
+*/
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+			// Build message to reply back
+			$messages1 = ['type' => 'text','text' => $text];	
+			//$messages2 = ['type' => 'sticker','packageId' => 1,'stickerId'=>1 ];
+			$messages2['type']='sticker';
+			$messages2['packageId']=1;
+			$messages2['stickerId']=1;
+			//$messages3 = ['type' => 'template','altText' => 'ohno','template'=> ['type'=>'confirm','text'=>'Are you sure?','actions'=>['type'=>'message','label'=>'yes','text'=>'yes']]];
+$action['type']='message';
+$action['label']='yes';
+$action['text']='yes';
+$action2['type']='message';
+$action2['label']='no';
+$action2['text']='no';
+$m['type']='confirm';
+$m['text']='Are you sure?';
+$m['actions']=array($action,$action2);
+$messages3['type'] = 'template';
+$messages3['altText'] ='check in mobile';
+$messages3['template']=$m;
+			
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = ['replyToken' => $replyToken,'messages' => [$messages1,$messages2,$messages3]];
+/*
+{
+  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+  "type": "message",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "user",
+    "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+  },
+  "message": {
+    "id": "325708",
+    "type": "text",
+    "text": "Hello, world"
+  }
+}
+*/
+			$post = json_encode($data);
+			//$post='{  "replyToken": "'.$replyToken.'","messages":{"type": "text","text": "Hello, world"}}';
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			echo $result . "\r\n";
+		}//end if
+	}//foreach
+}//end if
+echo "OK";
+?>
