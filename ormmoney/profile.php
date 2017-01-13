815517fefb00b8fdb5a64fcc1d40d15336ff5a8f
ormbook <ormbook@gmail.com>
2017-01-13 15:35:48 +0700

    Create profile.php

---
+++ ormmoney/profile.php
@@ -0,0 +1,13 @@
+<?php require_once("var_token.inc.php");?>
+<?php
+
+
+$url = 'https://api.line.me/v1/profile';
+$headers = array('Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result;
