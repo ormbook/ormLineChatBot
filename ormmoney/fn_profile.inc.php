ffa3f5f0df5160ff3857dad5c6930c82691f82ba
ormbook <ormbook@gmail.com>
2017-01-13 16:03:31 +0700

    Update fn_profile.inc.php

--- ormmoney/fn_profile.inc.php
+++ ormmoney/fn_profile.inc.php
@@ -12,9 +12,9 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
-  //$arrProfile=json_decode($result);
-  //return $arrProfile;
-  return $result;
+  $arrProfile=json_decode($result);
+  return $arrProfile;
+  //return $result;
   
   /*
   $arrProfile=fn_profile($userId);
