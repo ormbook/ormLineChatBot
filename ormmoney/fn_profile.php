39889a681e27fd1b899805d0e9eac93037f70371
ormbook <ormbook@gmail.com>
2017-01-13 15:40:04 +0700

    Update and rename profile.php to fn_profile.php

--- ormmoney/fn_profile.php
+++ ormmoney/fn_profile.php
@@ -1,8 +1,10 @@
 <?php require_once("var_token.inc.php");?>
 <?php
 
-
-$url = 'https://api.line.me/v1/profile';
+function fn_profile($userId){
+  global $access_token;
+//$url = 'https://api.line.me/v1/profile';
+$url="https://api.line.me/v2/bot/profile/".$userId;
 $headers = array('Authorization: Bearer ' . $access_token);
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -10,4 +12,5 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
-echo $result;
+return $result;
+}
