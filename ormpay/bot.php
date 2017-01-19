4e03566bd751faee0cd8aacc5dba54ddad657965
ormbook <ormbook@gmail.com>
2017-01-19 17:58:07 +0700

    Update bot.php

--- ormpay/bot.php
+++ ormpay/bot.php
@@ -69,7 +69,8 @@ if (!is_null($events['events'])) {
 				
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = ['replyToken' => $replyToken,'messages' => [$messages1,$messages2,$messages3]];
+			//$data = ['replyToken' => $replyToken,'messages' => [$messages1,$messages2,$messages3]];
+			$data = ['replyToken' => $replyToken,'messages' => [$messages1]];
 /*
 {
   "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
