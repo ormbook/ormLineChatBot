ce3b37afd71eb8435372ae4450afd9f98bf75144
ormbook <ormbook@gmail.com>
2017-02-06 23:19:39 +0700

    Update bot.php

--- ormcoin/bot.php
+++ ormcoin/bot.php
@@ -102,7 +102,7 @@ if (!is_null($events['events'])) {
 				$arrProfile=fn_profile($userId);	
 				$messages1 = ['type' => 'image','originalContentUrl' => $arrProfile[pictureUrl],'previewImageUrl'=>'https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Creative-Tail-Objects-mirror.svg/200px-Creative-Tail-Objects-mirror.svg.png'];
 				$messages2 = ['type' => 'text','text' => 'Hello:'.$arrProfile[displayName]];
-				$messages2 = ['type' => 'text','text' => ''.$arrProfile[statusMessage]];
+				$messages3 = ['type' => 'text','text' => ''.$arrProfile[statusMessage]];
 				break;
 					
 				default :
